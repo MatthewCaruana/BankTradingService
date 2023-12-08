b4f55669c2a42f0fde546c2c2f0298e81463e04d
MatthewCaruana <matthew.caruana99@gmail.com>
2023-12-08 16:45:37 +0100

    Created Consumer Message Queue with basic setup

--- BankTradingService.Consumer/Program.cs
+++ BankTradingService.Consumer/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        // Setup Consumer using Kafka and subscribe to publishing service
+        ConsumerConfig config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            ClientId = "BankTradingServiceClient",
+            GroupId = "BankTradingServiceGroup",
+            BrokerAddressFamily = BrokerAddressFamily.V4
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe("BankTradingService");
+
+        // Loop infinitely in order to be able to capture any incoming messages in message queue
+        while (true)
+        {
+            // Will wait for a message to consume and display the message
+            var consumeResult = consumer.Consume();
+            Console.WriteLine($"Message received from {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+        }
+
+        consumer.Close();
+        Console.ReadLine();
+    }
+}
