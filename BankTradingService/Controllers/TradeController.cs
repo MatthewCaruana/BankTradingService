5d965b57e17bbd27b83254b3ab5285d0ba1e30c5
MatthewCaruana <matthew.caruana99@gmail.com>
2023-12-09 09:29:01 +0100

    Created TradeController

---
+++ BankTradingService/Controllers/TradeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankTradingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TradeController : ControllerBase
+    {
+        private readonly ILogger<TradeController> _logger;
+
+        public TradeController(ILogger<TradeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetUserTrades")]
+        public void GetUserTrades(int userID)
+        {
+            
+        }
+
+        [HttpPost]
+        [Route("ExecuteTrade")]
+        public void ExecuteTrade()
+        {
+
+        }
+    }
+}
