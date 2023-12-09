29fcd81575a3069e0a0d78dd2fc9661c9471511e
MatthewCaruana <matthew.caruana99@gmail.com>
2023-12-09 14:24:49 +0100

    Addition of user retrieval

---
+++ BankTradingService.Data/Repositories/Interface/IUserTradeRepository.cs
@@ -0,0 +1,14 @@
+﻿using BankTradingService.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTradingService.Data.Repositories.Interface
+{
+    public interface IUserTradeRepository
+    {
+        UserDataModel? GetUserByID(int id);
+    }
+}
---
+++ BankTradingService.Data/Repositories/UserTradeRepository.cs
@@ -0,0 +1,26 @@
+﻿using BankTradingService.Data.Context.Interface;
+using BankTradingService.Data.Models;
+using BankTradingService.Data.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTradingService.Data.Repositories
+{
+    public class UserTradeRepository : IUserTradeRepository
+    {
+        private ITradeDbContext _dbContext;
+
+        public UserTradeRepository(ITradeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public UserDataModel? GetUserByID(int id)
+        {
+            return _dbContext.User.SingleOrDefault(x => x.Id == id);
+        }
+    }
+}
